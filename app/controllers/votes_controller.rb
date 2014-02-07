571062359630d822a1db3ece30497cbc9b710e44
Greg Hauenstein <greghauenstein@gmail.com>
2014-02-07 10:29:35 -0600

    Completed voting functionality.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @vote = current_user.votes.where(:scantory_id => params[:vote][:story_id]).first || current_user.votes.create(params[:vote])
+    @vote = current_user.votes.where(:story_id => params[:vote][:story_id]).first || current_user.votes.create(params[:vote])
     @vote.update_attributes(:up => params[:vote][:up])
     redirect_to :back
   end
